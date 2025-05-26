89ceb8203cd6a9dc70addef8d5309de1037bb22f
Omkar Milind Zende <expertomkar@gmail.com>
2025-05-26 10:55:02 +0530

    Update server.js
    added environment variable from frontend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 // Routes
 //app.use("/api/auth", require("./routes/authRoutes"));
